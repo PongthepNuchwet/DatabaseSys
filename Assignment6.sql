53d53ed1fb8c616123ea924c86592e715076c3f5
PongthepNuchwet <jt061220@gmail.com>
2023-01-25 12:01:30 +0700

    Asignmanet 6

--- Assignment6.sql
+++ Assignment6.sql
@@ -1,50 +1,3 @@
--- Q01 Find all sailors
-SELECT sid, sname, rating, age
-FROM sailors
-;
- sid |  sname  | rating | age  
------+---------+--------+------
-  22 | Dustin  |      7 | 45.0
-  29 | Brutus  |      1 | 33.0
-  31 | Lubber  |      8 | 55.5
-  32 | Andy    |      8 | 25.5
-  58 | Rusty   |     10 | 35.0
-  64 | Horatio |      7 | 35.0
-  71 | Zorba   |     10 | 16.0
-  74 | Horatio |      9 | 35.0
-  85 | Art     |      3 | 25.5
-  95 | Bob     |      3 | 63.5
-(10 rows)
-
--- Q02 Find all reserves.
-SELECT sid, bid, day
-FROM reserves
-;
- sid | bid |    day     
------+-----+------------
-  22 | 101 | 1998-10-10
-  22 | 102 | 1998-10-10
-  22 | 103 | 1998-08-10
-  22 | 104 | 1998-07-10
-  31 | 102 | 1998-10-11
-  31 | 103 | 1998-06-11
-  31 | 104 | 1998-12-11
-  64 | 101 | 1998-05-09
-  64 | 102 | 1998-08-09
-  74 | 103 | 1998-08-09
-(10 rows)
-
--- Q03 Find all boats.
-SELECT * FROM boats
-;
-bid |   bname   | color 
------+-----------+-------
- 101 | Interlake | blue
- 102 | Interlake | red
- 103 | Clipper   | green
- 104 | Marine    | red
-(4 rows)
-
 -- Q11 Find all sailors with a rating above 7.
 SELECT * FROM sailors WHERE sailors.rating > 7 ;
 
@@ -57,11 +10,9 @@ SELECT * FROM sailors WHERE sailors.rating > 7 ;
   32 | Andy    |      8 | 25.5
 (5 rows)
 
--- Q16, Q2 Find the sids and names of sailors who has reserved a read boat.
-SELECT S.sid, S.sname  FROM sailors S , reserves R, boats B  WHERE S.sid = R.sid AND R.bid = B.bid AND B.color = 'red' ;
-SELECT S.sid, S.sname  FROM sailors S JOIN reserves R ON S.sid = R.sid JOIN boats B  ON R.bid = B.bid WHERE B.color = 'red' ;
 
-;
+-- Q16, Q2 Find the sids and names of sailors who has reserved a read boat.
+SELECT S.sid, S.sname  FROM sailors S JOIN reserves R ON S.sid = R.sid JOIN boats B  ON R.bid = B.bid WHERE B.color = 'red' ;
  sid 
 -----
   22
@@ -70,22 +21,43 @@ SELECT S.sid, S.sname  FROM sailors S JOIN reserves R ON S.sid = R.sid JOIN boat
   64
   31
 (5 rows)
--- Q3 Find the colors of boats reserved by Lubber.
-SELECT B.bid ,B.color FROM sailors S JOIN reserves R ON S.sid = R.sid JOIN boats B  ON R.bid = B.bid WHERE S.sname = 'Lubber' ;
 
-;
- color 
--------
- red
- green
- red
+--Find sailors with ratting greater than the rating of at least one sailor called 'Horatio'
+SELECT * FROM sailors S 
+WHERE S.rating > ANY (SELECT S2.rating FROM sailors S2 WHERE S2.sname = 'Horatio');
+ sid |  sname  | rating | age  
+-----+---------+--------+------
+  31 | Lubber  |      8 | 55.5
+  58 | Rusty   |     10 | 35.0
+  71 | Zorba   |     10 | 16.0
+  74 | Horatio |      9 | 35.0
+  32 | Andy    |      8 | 25.5
+(5 rows)
+
+--Find sailors with ratting greater than the rating of all 25.5 year old sailors 
+SELECT *
+FROM sailors S 
+WHERE S.rating > ALL (SELECT S2.rating FROM sailors S2  WHERE S2.age = 25.5) ;
+ sid |  sname  | rating | age  
+-----+---------+--------+------
+  58 | Rusty   |     10 | 35.0
+  71 | Zorba   |     10 | 16.0
+  74 | Horatio |      9 | 35.0
 (3 rows)
 
+--Find all sailors who have not reserved a red boat
+SELECT S.sname
+FROM sailors S 
+LEFT JOIN (
+  SELECT R.sid , R.bid , B.color 
+  FROM reserves R
+  INNER JOIN boats B ON (R.bid=B.bid)
+  WHERE B.color = 'red'
+  ) AS RB ON S.sid=RB.sid 
+WHERE RB.color IS NULL ; 
 
 -- Q17 Find the names of sailors whow have sailed two different boats on the same day.
-SELECT S.sname,COUNT(R.day) FROM sailors S JOIN reserves R ON S.sid = R.sid JOIN boats B  ON R.bid = B.bid WHERE S.sname = 'Lubber' ;
-
-
+SELECT S.sname FROM sailors S JOIN reserves R1 ON R1.sid = S.sid JOIN reserves R2 ON R2.sid = S.sid WHERE R1.sid = R2.sid AND R1.day = R2.day AND R1.bid <> R2.bid ;
 ;
  sname  
 --------
@@ -94,9 +66,14 @@ SELECT S.sname,COUNT(R.day) FROM sailors S JOIN reserves R ON S.sid = R.sid JOIN
 (2 rows)
 
 -- Q20 Find all sids of sailors who have a rating of 10 or reserved boat 104
-SELECT
+SELECT S.sid FROM sailors S WHERE S.rating = 10 
+UNION
+SELECT R.sid FROM reserves R WHERE R.bid = 104 ;
 
-;
+SELECT S10.sid , R104.sid 
+FROM (SELECT S.sid FROM sailors S WHERE S.rating = 10) S10 
+FULL OUTER JOIN (SELECT r.sid FROM reserves r WHERE r.bid=104) R104 ON S10.sid=R104.sid ;
+
  sid 
 -----
   31
@@ -105,41 +82,20 @@ SELECT
   58
 (4 rows)
 
--- Q21 Find the names of sailors who have hot reserved a red boat
-SELECT
+-- Find name and age of the oldest sailor(s)
+SELECT S.sname ,S.age FROM sailors S WHERE S.rating <= ALL (SELECT S2.rating FROM sailors S2) ;
 
-;
-  sname  
----------
- Brutus
- Rusty
- Zorba
- Horatio
- Art
- Bob
- Andy
-(7 rows)
 
--- Q28 Count the number of sailors
-SELECT
+-- The sailor with highest rating 
+SELECT * FROM sailors S 
+WHERE S.rating >= ALL (SELECT S2.rating FROM sailors S2) ;
 
-;
- count 
--------
-    10
-(1 row)
+SELECT * FROM sailors S 
+WHERE S.rating = (SELECT MAX(S2.rating) FROM sailors S2) ;
 
--- Q29 Count the number of different sailor names
-SELECT
-
-;
- count 
--------
-     9
-(1 row)
 
 -- Q30 Find the names of sailors who are older than the oldest sailor with a rating of 10
-SELECT
+SELECT S.sname FROM sailors S WHERE S.age > (SELECT MAX(S2.age) FROM sailors S2 WHERE S2.rating=10 );
 
 ;
 sname  
@@ -150,72 +106,38 @@ sname
 (3 rows)
 
 -- Q31 Find the age of the youngest sailor for each rating level
-SELECT
-
-;
- rating | min  
---------+------
-      9 | 35.0
-      3 | 25.5
-     10 | 16.0
-      7 | 35.0
-      1 | 33.0
-      8 | 25.5
+SELECT S.rating , MIN(S.age) ,AVG(S.age)::numeric(3,1) FROM sailors S GROUP BY  S.rating ; 
+ rating | min  | avg  
+--------+------+------
+      9 | 35.0 | 35.0
+      3 | 25.5 | 44.5
+     10 | 16.0 | 25.5
+      7 | 35.0 | 40.0
+      1 | 33.0 | 33.0
+      8 | 25.5 | 40.5
 (6 rows)
 
--- Q32 Find the sage of the youngest sailor who is eligible to vote (i.e., is at least 18 years old) for each rating level with at least two such sailors.
-SELECT
+-- Find all pairs of sam-color bost 
+SELECT B1.bname , B2.bname FROM boats B1 , boats B2 WHERE B1.color = B2.color AND B1.bname <> B2.bname ;
+SELECT * FROM boats B1 INNER JOIN boats B2 ON B1.color = B2.color WHERE B1.bid <> B2.bid ;  
 
-;
- rating | minage 
---------+--------
-      3 |   25.5
-      7 |   35.0
-      8 |   25.5
-(3 rows)
-
--- Q33 For each red boat, find the number of reservations for this boat
-SELECT
-
-;
- bid | reservationcount 
------+------------------
- 102 |                3
- 104 |                2
+   bname   |   bname   
+-----------+-----------
+ Interlake | Marine
+ Marine    | Interlake
 (2 rows)
 
+-- Find sailor who have reserved all boats 
+SELECT S.sname 
+FROM sailors S 
+WHERE NOT EXISTS (SELECT B.bid 
+                  FROM boats B 
+                  WHERE NOT EXISTS (SELECT R.bid 
+                                    FROM reserves R 
+                                    WHERE R.bid = B.bid 
+                                    AND R.sid = S.sid)) ;
 
--- Q34 Find the average age of sailors for each rating level that has at least two sailors
-SELECT
-
-;
- rating |       avgage        
---------+---------------------
-      3 | 44.5000000000000000
-     10 | 25.5000000000000000
-      7 | 40.0000000000000000
-      8 | 40.5000000000000000
-(4 rows)
-
--- Q35 Find the average age of sailors who are of voting age (i.e. at least 18 years old) for each rating level that has at least two sailors.
-SELECT
-
-;
- rating |       avgage        
---------+---------------------
-      3 | 44.5000000000000000
-     10 | 35.0000000000000000
-      7 | 40.0000000000000000
-      8 | 40.5000000000000000
-(4 rows)
-
--- Q36 Find the average age of sailors who are of voting age (i.e., at least 18 years old) for each rating level that has at least two such sailors
-SELECT
-
-;
- rating |       avgage        
---------+---------------------
-      3 | 44.5000000000000000
-      7 | 40.0000000000000000
-      8 | 40.5000000000000000
-(3 rows)
+ sname  
+--------
+ Dustin
+(1 row)
